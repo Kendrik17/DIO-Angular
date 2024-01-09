0ab4672009aee3edbce4a6138c91f0a3bc4bc797
Kendrik17 <106797081+Kendrik17@users.noreply.github.com>
2024-01-09 17:41:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-bswwlz
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Kendrik17/stackblitz-starters-bswwlz)
---
+++ src/app/components/menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'menu-component',
+  template: '<h2>meu ovo esquerdo</h2>',
+  styles: ['h2{ color: black}']
+})
+export class MenuComponent {}
